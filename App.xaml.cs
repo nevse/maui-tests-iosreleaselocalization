ba88aa632365a71e0be79ab7374063742055f804
nevse <nov.eugen@gmail.com>
2023-01-16 10:42:34 +0400

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace iOSReleaseLocalizationIssue;
+
+public partial class App : Application
+{    
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace iOSReleaseLocalizationIssue;
+
+public partial class MainPage : ContentPage
+{
+	System.Resources.ResourceManager resourceManager = new System.Resources.ResourceManager("iOSReleaseLocalizationIssue.Resources.MyLocalization", typeof(MainPage).Assembly);
+	
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnSetEngCulture(object sender, EventArgs e)
+	{
+		Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        CultureStringLabel.Text = resourceManager.GetString("MyString");
+	}
+	private void OnSetDeCulture(object sender, EventArgs e)
+	{
+		Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("de");
+        CultureStringLabel.Text = resourceManager.GetString("MyString");
+    }
+    private void OnSetEsCulture(object sender, EventArgs e)
+    {
+        Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = new CultureInfo("es");
+        CultureStringLabel.Text = resourceManager.GetString("MyString");
+    }
+}
+
